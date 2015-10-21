dcc582b3e14016157c299f0fdcb1a92fe07c0c45
warriorzhai <saisn@126.com>
2015-10-21 22:03:04 +0800

    添加函数测试update_db

--- stockAPI/test_databaseinterface.py
+++ stockAPI/test_databaseinterface.py
@@ -5,23 +5,30 @@ Created on Mon Oct 19 17:15:08 2015
 @author: warriorzhai
 """
 
-from DatabaseInterface import DatabaseInterface
+
 import os
-os.chdir('C:\Users\warriorzhai\Desktop\project\stockAPI\stockAPI')
+os.chdir('C:/Users/warriorzhai/Desktop/project/stockAPI/stockAPI')
+from DatabaseInterface import DatabaseInterface
 
 #更换数据库修改以下三项
 user_name='root'
-psw='shinan1988'
+psw='root'
 MONGODB_URI ='mongodb://%s:%s@ds041394.mongolab.com:41394/tempdb'
 
 MONGODB_URI=MONGODB_URI % (user_name,psw)
 dbin=DatabaseInterface(MONGODB_URI=MONGODB_URI)
-
-
-#测试connect_db
 db_name='tempdb'
 col_name='tempcol'
+
+#测试connect_db
 connection=dbin.connect_db(db_name,col_name)
 connection.count()
+
+#测试update_db
+clause={"fund_name":"test"}
+updates={"$set":{"fund_return":"2"}}
+dbin.update_db(db_name,col_name,clause,updates,upsert=False, multi=False)
+
+
 
 
